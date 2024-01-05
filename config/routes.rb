a6577e0a031ef9415d4a29ee831fdd88213dc6ab
Amber Torres <amber.torres@webpt.com>
2024-01-05 15:43:42 -0500

    add articles show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :articles
+  #This gives all the routes for articles, comes with scaffold but you'll need to manually add this here if you didn't use the scaffold
+  #can use :only to limit actions that are permitted to the resource
   root 'pages#home'
   get 'about', to: 'pages#about' #A get request to the "about page" handled by the pages controller
 end
